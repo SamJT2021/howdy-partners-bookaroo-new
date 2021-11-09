eb217419f41802cbd536b76de968fe072984ab90
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-09 14:44:38 +0000

    HP_feature_11: Added test for like button, added column to posts database, updated newsfeed erb

---
+++ db/migrate/20211109134600_add_likes_to_posts.rb
@@ -0,0 +1,5 @@
+class AddLikesToPosts < ActiveRecord::Migration[6.1]
+  def change
+    add_column :posts, :number_of_likes, :integer
+  end
+end
---
+++ spec/features/user_can_react_to_posts_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Adding reactions", type: :feature do
+  fixtures :posts
+
+  scenario "user can add a comment to a post" do
+    sign_up_test_user
+    visit "/newsfeed"
+    fill_in "comment-post-id-1", with: "This is a comment!"
+    click_button "comment-button-id-1"
+    expect(page).to have_current_path('/newsfeed')
+    expect(page).to have_content('This is a comment!')
+  end
+
+  scenario "user can add a like to a post" do
+    sign_up_test_user
+    visit "/newsfeed"
+    expect(find_by_id("like-count-id-1")).to have_content("Likes: 0")
+    click_button "like-button-id-1"
+    expect(find_by_id("like-count-id-1")).to have_content("Likes: 1")
+  end
+
+  # Do we have an "unlike" button that reverses the like?
+  # Do we use the post-id-1 to like a certain post, then test it with a second user, so Likes: 2?
+
+end
