501aeb00208ff286b1fc2c44b610ccd7aabb9163
Steve Jones <steve.jones763@gmail.com>
2021-11-09 16:24:54 +0000

    Newsfeed displays message when there are no posts.

--- spec/features/a_message_is_displayed_when_there_are_no_posts_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "Newsfeed", type: :feature do
-  scenario "Can submit posts and view them" do
-    sign_up_test_user
-    visit "/newsfeed"
-    expect(page).to have_content("There are no posts to show")
-  end
-end
