9dd8737dcde2082627563194bf69f3b5cb9b3c7e
Beth Lambert-Matthews <blambertmatthews@gmail.com>
2021-11-11 14:28:52 +0000

    HP_general_03_v2: Updated test for default profile picture

--- spec/features/users_can_sign_up_spec.rb
+++ spec/features/users_can_sign_up_spec.rb
@@ -34,7 +34,7 @@ RSpec.feature "Sign up", type: :feature do
 
   scenario "users who do not enter a profile picture are assigned a default image" do
     sign_up_test_user_without_profile_picture
-    expect(page).to have_css("img[src*='https://lh3.googleusercontent.com/proxy/o4g3witOQP5IKgMnR271nEd2NwM-CetBeNdsl5z_Mozw-wrAXi30uBNL4-ckj4dFNA5Z_BsZ6fLDMVa0AZBg_xhw9A']")
+    expect(page).to have_css("img[src*='https://cdn.pixabay.com/photo/2014/04/03/10/40/cowboy-311087_960_720.png']")
   end
 
 end
