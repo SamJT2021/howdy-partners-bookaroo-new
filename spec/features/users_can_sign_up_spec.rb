6700872df037f39ff9c3708c85c3ee5bd99dd4a3
Sam Thoburn <samuel.j.thoburn@outlook.com>
2021-11-11 11:39:10 +0000

    HP_feature_17: Moved feature test into the sign up feature test file

--- spec/features/users_can_sign_up_spec.rb
+++ spec/features/users_can_sign_up_spec.rb
@@ -14,6 +14,16 @@ RSpec.feature "Sign up", type: :feature do
     expect(find_by_id('form-username')).to have_content "can't be blank"
     expect(find_by_id('form-password')).to have_content "can't be blank"
   end
+
+  scenario "User cannot create an account with already existing username" do
+    sign_up_test_user
+    visit('/sign-up')
+    fill_in "user[username]", with: "user1"
+    fill_in "user[password]", with: "test"
+    click_button "Sign up"
+    expect(page).to have_current_path("/users")
+    expect(page).to have_content("Username has already been taken")
+  end
   
   scenario "New users can fill in the sign up form" do
     visit "/"
